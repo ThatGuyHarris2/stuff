fe6ef749455ddc868ee70606d770c2646ce251be
ThatGuyHarris2 <31483906+ThatGuyHarris2@users.noreply.github.com>
2017-10-31 23:49:38 -0400

    Create etch_a_sketch_input.ino

---
+++ etch_a_sketch_input.ino
@@ -0,0 +1,18 @@
+void setup() {
+  Serial.begin(9600);
+  pinMode(7, INPUT_PULLUP);
+}
+
+void loop() {
+  int potVal = analogRead(A0);
+  int lightVal = analogRead(A1);
+  int buttonVal = digitalRead(7);
+  int buttonVal2 = digitalRead(8);
+
+  Serial.print(potVal);
+  Serial.print(',');
+  Serial.print(lightVal);
+  Serial.print(',');
+  Serial.print(buttonVal);
+  Serial.println();
+}
