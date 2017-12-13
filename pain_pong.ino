e7ca86ccd72d05859f6b4124b8b4109ba12ed1f9
ThatGuyHarris2 <31483906+ThatGuyHarris2@users.noreply.github.com>
2017-12-13 03:20:11 -0500

    Update pain_pong.ino

--- pain_pong.ino
+++ pain_pong.ino
@@ -19,13 +19,12 @@ void loop() {
   if (Serial.available() > 0) {
     char data = Serial.read();
 
-    // if it reads the char value from the p5 code, move servo
     if (data == 'R') {
       servoL.write(90);
       delay(500);
       servoL.write(0);
       delay(100);
-    } else { 
+    } else if(data == 'L') {
       servoR.write(90);
       delay(500);
       servoR.write(0);
